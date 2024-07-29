ce1d842bdf82c27f60b37308ced6aa709fb8725c
DanyaGTx <raxub007@gmail.com>
2024-07-29 11:57:51 +0200

    fixed warning related to fetching data and moved header date to seperated component

--- services/orders.ts
+++ services/orders.ts
@@ -2,13 +2,12 @@ export const useOrdersService = () => {
   const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
   const getAllOrders = async () => {
-    const { data, error } = await useFetch(`${apiBaseUrl}/api/orders/`);
-
-    if (error.value) {
+    try {
+      const response = await $fetch(`${apiBaseUrl}/api/orders/`);
+      return response;
+    } catch (error) {
       throw new Error("Failed to fetch data");
     }
-
-    return data.value;
   };
 
   return {
--- services/products.ts
+++ services/products.ts
@@ -2,13 +2,12 @@ export const useProductsService = () => {
   const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
   const getAllProducts = async () => {
-    const { data, error } = await useFetch(`${apiBaseUrl}/api/products/`);
-
-    if (error.value) {
+    try {
+      const response = await $fetch(`${apiBaseUrl}/api/products/`);
+      return response;
+    } catch (error) {
       throw new Error("Failed to fetch data");
     }
-
-    return data.value;
   };
 
   return {
